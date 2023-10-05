cd2589cdb890e3f467fcc2e1c6b233c42887bc31
Марина <marina.morozevich@gmail.com>
2023-10-05 16:01:49 +0300

    _Main moved to _main

--- src/main/java/ru/morozevich/builderapp/_main/_Main.java
+++ src/main/java/ru/morozevich/builderapp/_main/_Main.java
@@ -1,10 +1,10 @@
-package org.example;
+package ru.morozevich.builderapp._main;
 
 import ru.morozevich.builderapp.model.entity.User;
 
 import java.util.ArrayList;
 
-public class Main {
+public class _Main {
     public static void main(String[] args) {
         ArrayList <User> userList = new ArrayList<>(20);
         for (int i = 0; i < 20; i++) {
