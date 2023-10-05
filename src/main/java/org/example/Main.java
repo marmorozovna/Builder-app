26c5da290475bca23441287ae4b1434742748c4e
Марина <marina.morozevich@gmail.com>
2023-10-05 15:55:01 +0300

    created userlist

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import ru.morozevich.builderapp.model.entity.User;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList <User> userList = new ArrayList<>(20);
+        for (int i = 0; i < 20; i++) {
+        userList.add(new User());
+        }
     }
 }
