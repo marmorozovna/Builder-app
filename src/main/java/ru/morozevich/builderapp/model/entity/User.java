b3b7a91eebad6459f063ba2031adfda06a51f8ea
Марина <marina.morozevich@gmail.com>
2023-10-05 15:46:13 +0300

    created User

---
+++ src/main/java/ru/morozevich/builderapp/model/entity/User.java
@@ -0,0 +1,67 @@
+package ru.morozevich.builderapp.model.entity;
+
+public class User {
+    private Long id;
+    private String login;
+    private StringBuilder password;
+    private int age;
+
+    public User() {
+    }
+
+    public User(Long id, String login, StringBuilder password, int age) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getLogin() {
+        return this.login;
+    }
+
+    public StringBuilder getPassword() {
+        return this.password;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public static Builder newBuilder() {
+        return new User().new Builder();
+    }
+
+    public class Builder {
+        private Builder() {
+        }
+
+        public Builder setId(Long id) {
+            User.this.id = id;
+            return this;
+        }
+
+        public Builder setLogin(String Login) {
+            User.this.login = login;
+            return this;
+        }
+
+        public Builder setPassword(StringBuilder password) {
+            User.this.password = password;
+            return this;
+        }
+
+        public Builder setAge(int age) {
+            User.this.age = age;
+            return this;
+        }
+
+        public User build() {
+            return User.this;
+        }
+    }
+}
